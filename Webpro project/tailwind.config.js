e263e48349d75a33d1dbb3b940de3eb04655cf99
PichayaPhatcharoen <109953311+PichayaPhatcharoen@users.noreply.github.com>
2024-02-26 18:21:34 +0700

    customer&employeeOrderviewPage

---
+++ Webpro project/tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ["./desktop/Webpro project/cus_order.{html,js}","./desktop/Webpro project/emp_order.{html,js}"],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+}
