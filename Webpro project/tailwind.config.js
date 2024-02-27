aaeeaef4b59d49634d9bf1734f01d6a59fa3ae81
PichayaPhatcharoen <109953311+PichayaPhatcharoen@users.noreply.github.com>
2024-02-28 06:24:06 +0700

    Delete Webpro project directory

--- Webpro project/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  content: ["./desktop/Webpro project/cus_order.{html,js}","./desktop/Webpro project/emp_order.{html,js}"],
-  theme: {
-    extend: {
-    },
-  },
-  plugins: [],
-}
